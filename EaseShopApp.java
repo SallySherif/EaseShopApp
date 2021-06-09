42a5db0c5c8335ac440e69a843b3c1551a14276f
SallySherif <55048532+SallySherif@users.noreply.github.com>
2021-06-10 01:09:25 +0200

    Add files via upload

---
+++ EaseShopApp.java
@@ -0,0 +1,11 @@
+package com.companyname.easeshop;
+
+import android.app.Application;
+
+public class EaseShopApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
